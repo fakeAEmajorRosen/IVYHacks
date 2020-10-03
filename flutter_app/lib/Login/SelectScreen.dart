73241285146f3aefe22f7f119ed8a616dcc96068
fakeAEmajorRosen <tyu304@gatech.edu>
2020-10-03 18:41:51 -0400

    app

---
+++ flutter_app/lib/Login/SelectScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_app/utilities/c.dart';
+
+class SelectScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+}
